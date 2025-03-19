e03c63f72bbd35d86735fdb974f8c37693a53f0d
XxMiguelxX <bustillosmiguel04@gmail.com>
2025-03-19 01:22:45 -0600

    subida de la nave con su animación funcionando

--- DeepSwim/Assets/scripts/playercontroler.cs
+++ DeepSwim/Assets/scripts/playercontroler.cs
@@ -10,10 +10,15 @@ public class playercontroler : MonoBehaviour
     public float fuerzaVuelo = 3f;
     public float gravedad = 9f;
     private Rigidbody2D rb;
+    private Animator Animator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        Animator = GetComponent<Animator>();
+        //        rb.gravityScale = 0; // Desactiva la gravedad de Unity
+
+
     }
 
     void Update()
@@ -21,12 +26,28 @@ public class playercontroler : MonoBehaviour
         if (Input.GetMouseButton(0))  // Mientras esté presionado el clic izquierdo
         {
             rb.velocity = new Vector2(rb.velocity.x, fuerzaVuelo);
+            Animator.SetBool("subida", true);
         }
         else
         {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y - gravedad * Time.deltaTime);
+            rb.velocity += new Vector2(0, -gravedad * Time.deltaTime);
+            Animator.SetBool("subida", false); // Se apaga cuando sueltas el clic
         }
     }
+
+    /* void Update()
+     {
+         if (Input.GetMouseButton(0))  // Mientras esté presionado el clic izquierdo
+         {
+             rb.velocity = new Vector2(rb.velocity.x, fuerzaVuelo);
+            // Animator.SetBool("subida", fuerzaVuelo != 0.0f);
+         }
+
+         else
+         {
+             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y - gravedad * Time.deltaTime);
+         }
+     } */
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
