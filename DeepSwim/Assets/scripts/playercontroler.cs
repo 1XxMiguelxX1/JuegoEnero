a3e6148dc3034c6d3716a67f7c19678763619209
XxMiguelxX <bustillosmiguel04@gmail.com>
2025-03-19 02:09:15 -0600

    subida y bajada medio funcionando

--- DeepSwim/Assets/scripts/playercontroler.cs
+++ DeepSwim/Assets/scripts/playercontroler.cs
@@ -27,13 +27,42 @@ public class playercontroler : MonoBehaviour
         {
             rb.velocity = new Vector2(rb.velocity.x, fuerzaVuelo);
             Animator.SetBool("subida", true);
+            Animator.SetBool("bajada", false);
         }
         else
         {
             rb.velocity += new Vector2(0, -gravedad * Time.deltaTime);
-            Animator.SetBool("subida", false); // Se apaga cuando sueltas el clic
+
+            // Si la velocidad en Y es aproximadamente 0, cambiar a idle
+            if (Mathf.Abs(rb.velocity.y) < 0.1f)
+            {
+                Animator.SetBool("subida", false);
+                Animator.SetBool("bajada", false);
+            }
+            // Si la velocidad es negativa, activar la animación de bajada
+            else if (rb.velocity.y < 0)
+            {
+                Animator.SetBool("bajada", true);
+                Animator.SetBool("subida", false);
+            }
         }
     }
+
+    /*  void Update()
+      {
+          if (Input.GetMouseButton(0))  // Mientras esté presionado el clic izquierdo
+          {
+              rb.velocity = new Vector2(rb.velocity.x, fuerzaVuelo);
+              Animator.SetBool("subida", true);
+          }
+          else
+          {
+              rb.velocity += new Vector2(0, -gravedad * Time.deltaTime);
+              Animator.SetBool("bajada", true);
+
+               Animator.SetBool("subida", false); // Se apaga cuando sueltas el clic
+          }
+      } */
 
     /* void Update()
      {
