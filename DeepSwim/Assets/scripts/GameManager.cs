9fec61058eafeb6dec96cc49828192bfb8e32a9f
XxMiguelxX <bustillosmiguel04@gmail.com>
2025-03-21 04:31:46 -0600

    ahora si #*##!
    esa fregadera jodia la compilación, pero ya no

--- DeepSwim/Assets/scripts/GameManager.cs
+++ DeepSwim/Assets/scripts/GameManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
-using UnityEditor.VersionControl;
+//using UnityEditor.VersionControl;
 
 public class GameManager : MonoBehaviour
 {
